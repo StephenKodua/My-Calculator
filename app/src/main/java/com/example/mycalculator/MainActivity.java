61f35eb39e790c9c65383eef1cfec0020f01ad6e
StephenKodua <koduastephen9@gmail.com>
2023-02-18 23:54:46 -0500

    Added my project

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -175,6 +175,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        divisionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 
 
